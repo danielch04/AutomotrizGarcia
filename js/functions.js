7a2ea60106965b50b1e1e6ced6ac88edf5825372
danielch04 <62576139+danielch04@users.noreply.github.com>
2025-04-10 12:14:32 -0600

    Add files via upload

---
+++ js/functions.js
@@ -0,0 +1,31 @@
+
+$(window).scroll(function() {
+    var scrollTopButton = $('#scrollTopBtn');
+    if ($(this).scrollTop() > 50) {
+        scrollTopButton.fadeIn();
+    } else {
+        scrollTopButton.fadeOut();
+    }
+});
+
+$('#scrollTopBtn').click(function() {
+    $('html, body').animate({ scrollTop: 0 }, '600');
+    return false;
+});
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const navLinks = document.querySelectorAll('#navbarNav .nav-link');
+    const navbarCollapse = document.getElementById('navbarNav');
+
+    navLinks.forEach(function (link) {
+      link.addEventListener('click', function () {
+        // Solo se colapsa si está visible (en pantallas pequeñas)
+        if (window.getComputedStyle(navbarCollapse).display !== "none") {
+          new bootstrap.Collapse(navbarCollapse, {
+            toggle: true
+          }).hide();
+        }
+      });
+    });
+  });
+  si te envió un link de una pagina puedes revisarlo y darme opiniones?
